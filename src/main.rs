2fffc0b9f23334d173bca0c4af7aa0942b1d619c
dataphguy <iamnotdataph@proton.me>
2026-04-16 08:34:14 +0500

    Connecting Rust and Python via pyo3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+fn main() -> PyResult<()> {
+    println!("Hello, world! By Rust!");
+
+    Python::attach(|py| {
+        // Add to sys.path
+        let sys = py.import("sys")?;
+        sys.getattr("path")?.call_method1("append", ("./src/pyscripts",))?;
+
+        // Import
+        let test_module = py.import("testmodule")?;
+
+        let hello_msg = test_module.call_method1("hello_world", ())?;
+        let msg: String = hello_msg.extract()?;
+        println!("{}", msg);
+
+        // Canculate test
+        //let result = my_module.call_method1("calculate", (3, 4))?;
+        //let value: i32 = result.extract()?;
+        //println!("Python result: {}", value);
+        Ok(())
+    })
 }
---
+++ src/pyscripts/testmodule.py
@@ -0,0 +1,7 @@
+# testmodule.py
+def calculate(a, b):
+    return a ** 2 + b ** 2
+
+def hello_world():
+    print("Hello, world! By Python!")
+    return "All works!"
